dd3451d339b8e84576cbc2a4057d70c04c7b3af7
BMacVeigh <brittney.watkins1@outlook.com>
2024-11-18 13:34:30 -0800

    Add files via upload

---
+++ Project2.ps1
@@ -0,0 +1,25 @@
+﻿
+# Function to generate a random password
+function Generate-RandomPassword {
+    param (
+        [int]$length = 12
+    )
+
+    # Define character sets
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    $numbers = '0123456789'
+    $special = '!@#$%^&*()_+[]{}|;:,.<>?'
+
+    # Combine all characters
+    $allChars = $lowercase + $uppercase + $numbers + $special
+    $password = -join ((1..$length) | ForEach-Object { $allChars[(Get-Random -Minimum 0 -Maximum $allChars.Length)] })
+
+    return $password
+}
+
+# Generate a random password with a specified length
+$randomPassword = Generate-RandomPassword -length 16
+
+# Display the password
+Write-Host "Your random password is: $randomPassword"

74a2eab2548178176780d84c4002124d39a74287
Ian Simons <iansimons93@gmail.com>
2024-11-18 15:55:57 -0500

    setting up outline for project

--- Project1.ps1
+++ Project1.ps1
@@ -1,1 +1,16 @@
-﻿Write-host "Hello World" 
+﻿Write-host "Hello World"
+
+$joke = "command which callsAPI"
+
+<#this is to help you learn to parse out the json you receive from the API#>
+write-host $joke
+
+<#here you should parse the json into question and answer#>
+
+$question = "question"
+$answer = "answer"
+
+write-host $question
+sleep 5
+<#find a way to set up sleep until a key is pressed#>
+write-host $answer

71b826a78984df865bb8d627f7e7223a00ad3a89
BMacVeigh <brittney.watkins1@outlook.com>
2024-11-19 13:40:56 -0500

    Update Inspiration.ps1

--- Inspiration.ps1
+++ Inspiration.ps1
@@ -17,19 +17,6 @@ Write-host "Good Morning! Here's your motivational message for the day:"
 Write-Host $randomMessage
 Write-Host ""
 
-#Check if the tasks file exists
-if (Test-Path $tasksFilePath) {
-    #Read the tasks from the file 
-    $tasks = Get-Content -Path $tasksFilePath
-
-    #Display the tasks 
-    Write-Host "Here are your tasks for the day:"
-    foreach (task in $tasks( {
-        Write-Host "- $task"
-    }
-} else {
-    Write-Host "You don't have any tasks listed for today."
-}
 
 
 
